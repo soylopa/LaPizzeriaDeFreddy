2bd3af611e257d6d453bdc195bb6b9d6262bd92e
Bubbl3Waffl3 <salahlfd0@gmail.com>
2021-11-18 14:30:01 +0100

    trying to inner html the whole main

--- Public/js/pizza-info.js
+++ Public/js/pizza-info.js
@@ -1,125 +1,165 @@
-let numero = 1
+let numero = 0
+
+let pizzas = [
+    {
+        id: 1,
+        nombre: "PIZZA PEPPERONI",
+        subtitulo: "PIZZA OF THE MONTH",
+        descripcion: "Mild and creamy cheese, black olives, zesty, pepperoni & our <br>signature Italian-Style Pizza Sauce.",
+        precio: 14.99,
+        imagen: "laimagen"
+    },
+    {
+        id: 2,
+        nombre: "PIZZA SWEET FREDDY",
+        subtitulo: "PIZZA OF THE MONTH",
+        descripcion: "Spiced tomato, onion, parsley, sausage, pickles, mozzarella.",
+        precio: 69.99,
+        imagen: "laimagen"
+    },
+    {
+        id: 3,
+        nombre: "PIZZA FIVE NIGHTS",
+        subtitulo: "PIZZA OF THE MONTH",
+        descripcion: "Tomato, olives, artichoke, onion, bacon, mozzarella, cheddar.",
+        precio: 42.09,
+        imagen: "laimagen"
+    },
+    {
+        id: 4,
+        nombre: "PARCERITO",
+        subtitulo: "PIZZA OF THE MONTH",
+        descripcion: "Mornay sauce base, mozzarella, crispy bacon, caramelized onion, supreme pineapple.",
+        precio: 104.99,
+        imagen: "laimagen"
+    },
+    {
+        id: 5,
+        nombre: "PIZZA DE CAVIAR",
+        subtitulo: "PIZZA OF THE MONTH",
+        descripcion: "Caviar.",
+        precio: 1.99,
+        imagen: "laimagen"
+    },
+    
+]
+
+let elmein = document.querySelector("main")
 
 
-// a.addEventListener("",function(){
-//     number = numero++;
-//     numberbefore = number-- 
-//     let pizzas = eval(`pizza${numero}info`)
-//     let pizzabefore = eval(`pizza${numberbefore}info`)
-//     pizzas.classList.toggle("show")
-//     pizzabefore.classList.toggle("show")
-//     console.log(numero)
-// })
-
-
-
-
-let nombrepizza = document.querySelector(".title-pizza");
-let descripcion = document.querySelector(".description");
-let precio = document.querySelector(".precio")
-let img1 = document.querySelector("img-pizza1")
-let quantity = document.querySelector(".q-number")
-let quantityvalue = quantity.value
-
-
-
-let nombrepizza1 = "<h1>PIZZA PEPPERONI</h1> <h2>PIZZA OF THE MONTH</h2>"
-let nombrepizza2 = "<h1>PIZZA SWEET FREDDY</h1> <h2>PIZZA OF THE MONTH</h2>"
-let nombrepizza3 = "<h1>PIZZA FIVE NIGHTS</h1> <h2>PIZZA OF THE MONTH</h2>"
-let nombrepizza4 = "<h1>PIZZA PARCERITO</h1> <h2>PIZZA OF THE MONTH</h2>"
-let descripcion1 = "Mild and creamy cheese, black olives, zesty, pepperoni & our <br>signature Italian-Style Pizza Sauce."
-let descripcion2 = "Spiced tomato, onion, parsley, sausage, pickles, mozzarella."
-let descripcion3 = "Tomato, olives, artichoke, onion, bacon, mozzarella, cheddar."
-let descripcion4 = "Mornay sauce base, mozzarella, crispy bacon, caramelized onion, supreme pineapple."
-let precio1 = "$14.99"
-let precio2 = "$69.99"
-let precio3 = "$42.09"
-let precio4 = "$104.99"
+function printhtml(index){
+    elmein.innerHTML = `<div class="pizza1 show">
+    <div class="title-pizza">
+        <h1>${pizzas[index].nombre}</h1> <h2>${pizzas[index].subtitulo}</h2>
+    </div>
+    <div class="info">
+        <div class="desc">
+            <div class="ingredients">
+                <h3>INFORMATION</h3>
+                <p class="description">${pizzas[index].descripcion}</p>
+                <button class="topping">
+                    + Add toppings
+                </button>       
+            </div>
+            <div class="quantity">
+                <h3>Quantity</h3>
+                <div class="quantity-buttons">
+                    <button class="q-minus">-</button>
+                    <input class="q-number" type="text" value="1"  readonly onmousedown="return false"></input>
+                    <button class="q-plus">+</button>
+                </div>
+            </div>
+        </div>
+        <div class="picking-size">
+            <div class="size-buttons">
+                <button class="size-s">S</button>
+                <button class="size-m active">M</button>
+                <button class="size-l">L</button>
+            </div>
+            <div class="pick-arrows">
+                <button class="scroll-right">
+                    <img src="./img/367376.svg" alt="flechita">
+                </button>
+                <button class="scroll-left">
+                    <img src="./img/367376.svg" alt="flechita">
+                </button>
+            </div>
+        </div>
+        <div class="pricing">
+            <h3>PRICE</h3>
+            <p class="precio">$${pizzas[index].precio}</p>
+            <button class="add-chart"><img src="img/iconobotonpricing.png">Add to box</button>
+        </div>
+    </div>
+</div>`
+}
 
 
 function adelantar() {
     numero++
-    numberbefore = numero-1
-    numberafter = numero+1
-    
-    let imagenpizza = document.querySelector("img-pizza" + numero);
-    let imagenpizzabefore = document.querySelector(`img-pizza${numberbefore}`);
-    let imagenpizzaafter = document.querySelector(`img-pizza${numberafter}`);
+    printhtml(numero)
 
-    if(numero>4){
-        numero = 1}
-    nombrepizza.innerHTML = eval(`nombrepizza${numero}`);
-    descripcion.innerHTML = eval(`descripcion${numero}`);
-    precio.innerHTML =  eval(`precio${numero}`);
-    // imagenpizza.classList.toggle("position1");
-    // imagenpizzabefore.classList.toggle("position1");
-    console.log(imagenpizza)
-    console.log(nombrepizza)
-    console.log(numberafter)
-
-    
+   
 }
+printhtml(0);
 
 function retroceder() {
     numero--;
-    if(numero<1){
-        numero = 4
-    }
-    nombrepizza.innerHTML = eval(`nombrepizza${numero}`)
-    descripcion.innerHTML = eval(`descripcion${numero}`)
-    precio.innerHTML =  eval(`precio${numero}`)
+    printhtml(numero)
+    
 }
 
 function togglesizel(){
     document.querySelector(".size-l").classList.toggle("active", true)
-    document.querySelector(".size-m").classList.toggle("active") == true ? document.querySelector(".size-m").classList.toggle("active") : console.log("a");
-    document.querySelector(".size-s").classList.toggle("active") == true ? document.querySelector(".size-s").classList.toggle("active") : console.log("a");
+    document.querySelector(".size-m").classList.toggle("active", false) 
+    document.querySelector(".size-s").classList.toggle("active", false)
 }
 function togglesizes(){
     document.querySelector(".size-s").classList.toggle("active", true)
-    document.querySelector(".size-m").classList.toggle("active") == true ? document.querySelector(".size-m").classList.toggle("active") : console.log("a");
-    document.querySelector(".size-l").classList.toggle("active") == true ? document.querySelector(".size-l").classList.toggle("active") : console.log("a");
+    document.querySelector(".size-m").classList.toggle("active", false) 
+    document.querySelector(".size-l").classList.toggle("active", false) 
 }
 function togglesizem(){
     document.querySelector(".size-m").classList.toggle("active", true)
-    document.querySelector(".size-s").classList.toggle("active") == true ? document.querySelector(".size-s").classList.toggle("active") : console.log("a");
-    document.querySelector(".size-l").classList.toggle("active") == true ? document.querySelector(".size-l").classList.toggle("active") : console.log("a");
+    document.querySelector(".size-s").classList.toggle("active", false)
+    document.querySelector(".size-l").classList.toggle("active", false)
 
 }
 
 function togglepizzas(){
     document.querySelector(".position1").classList.toggle("pizza-s", true)
-    document.querySelector(".position1").classList.toggle("pizza-m") == true ? document.querySelector(".position1").classList.toggle("pizza-m") : console.log("a");
-    document.querySelector(".position1").classList.toggle("pizza-l") == true ? document.querySelector(".position1").classList.toggle("pizza-l") : console.log("a");
+    document.querySelector(".position1").classList.toggle("pizza-m", false)
+    document.querySelector(".position1").classList.toggle("pizza-l", false)
 }
 
 function togglepizzal(){
     document.querySelector(".position1").classList.toggle("pizza-l", true)
-    document.querySelector(".position1").classList.toggle("pizza-m") == true ? document.querySelector(".position1").classList.toggle("pizza-m") : console.log("a");
-    document.querySelector(".position1").classList.toggle("pizza-s") == true ? document.querySelector(".position1").classList.toggle("pizza-s") : console.log("a");
-
+    document.querySelector(".position1").classList.toggle("pizza-m", false)
+    document.querySelector(".position1").classList.toggle("pizza-s", false)
 }
 
 function togglepizzam(){
     document.querySelector(".position1").classList.toggle("pizza-m", true)
-    document.querySelector(".position1").classList.toggle("pizza-s") == true ? document.querySelector(".position1").classList.toggle("pizza-s") : console.log("a");
-    document.querySelector(".position1").classList.toggle("pizza-l") == true ? document.querySelector(".position1").classList.toggle("pizza-l") : console.log("a");
-
+    document.querySelector(".position1").classList.toggle("pizza-s", false)
+    document.querySelector(".position1").classList.toggle("pizza-l", false)
 }
-
-
-
-document.querySelector(".scroll-right").addEventListener("click", adelantar)
-document.querySelector(".scroll-left").addEventListener("click", retroceder)
-document.querySelector(".q-plus").addEventListener("click",() => {
-document.querySelector(".q-number").value++
-})
-quantity.addEventListener("click",() => {
-    quantityvalue = (quantityvalue == 1) ? 1 : quantityvalue-1;
-})
 document.querySelector(".size-m").addEventListener("click",togglesizem)
 document.querySelector(".size-l").addEventListener("click",togglesizel)
 document.querySelector(".size-s").addEventListener("click",togglesizes)
 document.querySelector(".size-m").addEventListener("click",togglepizzam)
 document.querySelector(".size-l").addEventListener("click",togglepizzal)
 document.querySelector(".size-s").addEventListener("click",togglepizzas)
+
+
+document.querySelector(".scroll-right").addEventListener("click", adelantar)
+document.querySelector(".scroll-left").addEventListener("click", retroceder)
+
+let quantity = document.querySelector(".q-number")
+let quantityvalue = quantity.value
+
+document.querySelector(".q-plus").addEventListener("click",() => {
+document.querySelector(".q-number").value++
+})
+quantity.addEventListener("click",() => {
+    quantityvalue = (quantityvalue == 1) ? 1 : quantityvalue-1;
+})
